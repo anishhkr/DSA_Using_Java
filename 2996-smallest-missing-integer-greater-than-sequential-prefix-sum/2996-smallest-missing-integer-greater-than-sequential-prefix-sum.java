e329d3c9908d35d7b57fe51fa6823cd41ad3ac63
Anish Kumar <anish87875@gmail.com>
2026-08-18 00:14:49 +0530

    Add comments for clarity in missingInteger method

--- 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.java
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.java
@@ -1,15 +1,15 @@
 class Solution {
     public int missingInteger(int[] nums) {
-        int sum = nums[0];
+        int sum = nums[0]; // prefix sum start
         for(int i=1; i<nums.length; i++){
             if(nums[i] == nums[i-1]+1){
-                sum += nums[i];
-            }else break;
+                sum += nums[i]; // add if sequence continues
+            }else break; // stop if sequence breaks
         }
-        Set<Integer> set = new HashSet<>();
+        Set<Integer> set = new HashSet<>(); // store all nums
         for(int num: nums) set.add(num);
-        int x = sum;
-        while(set.contains(x)) x++;
+        int x = sum; // candidate answer
+        while(set.contains(x)) x++; // find first missing
         return x;
     }
 }
